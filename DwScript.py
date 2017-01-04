37ce871130f7102a92b1843c5f6c7085cb7062bd
Alex <dovudo98@gmail.com>
2017-01-04 05:24:19 +0300

    Rise to power, Changed agrsParser,help,version, add global comments, downloadin full boards

--- DwScript.py
+++ DwScript.py
@@ -1,3 +1,16 @@
+'''
+abuscript.py
+Version 1.10.8
+Require: Python2.7 and lower version
+It support all platforms(Linux, Windows, Mac)
+Usage: python abuscript.py https://2ch.hk/b/res/143636089.html | Easy way
+Usage: python abuscript.py -b e <-- 'boards' | To download full board
+Usage: python abuscript.py https://2ch.hk/b/res/143636089.html -w || -p || -g  | To download only webm|picture|gifs 
+May be in future GUI version 
+
+WARNING: Will downloaded to the directory with abuscript
+'''
+
 import argparse
 import os
 import re
@@ -5,7 +18,6 @@ import urllib2
 import time
 import json
 # Copyright
-#Function verify files exist
 
 def get_all_threads(board):
     catalog = urllib2.urlopen("https://2ch.hk/" + board + "/catalog.json").read()
@@ -21,6 +33,7 @@ def download_board(board):
         download_thread(item)
 
 def isExist(name_file):
+    #Function verify files exist
     if os.path.isfile(name_file):
         return True
     else:
@@ -49,42 +62,57 @@ def download_thread(url):
     folder = url.split("/")[-1][:-5]
     pattern = get_pattern()
     if not os.path.isdir(folder):
+        #Verify exist folder 
         os.makedirs(folder)
-        print "Create folder " + folder
+        print("Create folder " + folder)
     else:
-        print "Thread sleep updating"
-    thread = urllib2.urlopen(url)
-    thread_media = re.findall(r'href="([^"]*' + pattern + ")", thread.read().decode('utf-8'))
-    thread_media = fix_array(thread_media)
-    for i, item in enumerate(thread_media):
-        filename = item.split("/")[-1]
-        media_url = "https://2ch.hk" + item
-        if isExist(folder + "/" + filename):
-            continue
-        print "Downloading " + filename + " (" + str(i + 1) + " of " + str(len(thread_media)) + ")"
+        print("Searching")
+    try: 
+        thread = urllib2.urlopen(url)
+        thread_media = re.findall(r'href="([^"]*' + pattern + ")", thread.read().decode('utf-8'))
+        thread_media = fix_array(thread_media)
+        for i, item in enumerate(thread_media):
+            filename = item.split("/")[-1]
+            media_url = "https://2ch.hk" + item
+            if isExist(folder + "/" + filename):
+                continue
+        print("Downloading " + filename + " (" + str(i + 1) + " of " + str(len(thread_media)) + ")")
         download_file(media_url, folder)
-
-    #auto update in iteration
-    time.sleep(10)
-    print 'To the out, Press Ctrl + C'
-    download_thread(url)
-                            
+        
+        #auto update every 10 iterations 
+        time.sleep(10)
+        print('To the out, Press Ctrl + C')
+        download_thread(url)
+    except urllib2.URLError:
+        print('Thread not found')
+        exit()
+    except KeyboardInterrupt:
+        print('Stoped')
+        exit()
+    except Exception, e:
+        print('Excetion: ' + e)
+                           
 def fix_array(array):
+    #Checking for dublicat
     for i, item in enumerate(array):
         if i % 2 == 0:
             del array[i]
     return array
 
 if __name__ == '__main__':
-    ar = argparse.ArgumentParser("python abuscript.py https://2ch.hk/b/res/123405664.html",epilog="Easy-to-Use download webm's, pictures or gifs \n Files will downloaded in dir with abuscript")
-    ar.add_argument('--version',action='version', version='version 1.0')
+    ar = argparse.ArgumentParser("python abuscript.py full link",\
+    epilog="Example: python abuscript.py https://2ch.hk/b/res/143636089.html \n \
+    Easy-to-Use download webm's, pictures or gifs \n \
+    Files will downloaded in dir with script \n \
+    after full downloading,\ will monitoring for new files ")
+    ar.add_argument('--version',action='version', version='version 1.10.8')
     ar.add_argument('link', metavar='link',type=str,help="Thread link")
     ar.add_argument("-w","--webm",action="store_true",dest='webm_switch',default=False,help="Only webm's")
     ar.add_argument("-p","--picture",action="store_true",dest='picture_switch',default=False,help="Only pictures")
     ar.add_argument("-g","--gif",action="store_true",dest='gif_switch',default=False,help="Only gifs")
     ar.add_argument("-a",'--all',action="store_true", dest='all_switch',default=True,help="Download all files (Default)")
-    ar.add_argument('-b','--board',metavar="board",dest='board_switch',default="0",help='Download all threads from board \n Example abuscript -b e')
-    ar.add_argument('-t','--thread',action='store_true',dest='thread_switch',default=False,help='Download by number, only with -b (--board)')
+    ar.add_argument('-b','--board',metavar="board",dest='board_switch',default=0,help='Download all threads from board \n \
+     Example abuscript -b e')
     global args
     args = ar.parse_args()
     options = vars(args)
